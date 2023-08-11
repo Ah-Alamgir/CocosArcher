c09925af2f6c597f2fd7725200c1f270dd1aa723
ahalamgir16 <ahalamgir16@outlook.com>
2023-08-11 08:16:31 +0600

    cocos 3.7

--- assets/Script/shoot.ts
+++ assets/Script/shoot.ts
@@ -12,9 +12,7 @@ let maxRotationUp = 47;
 
 @ccclass('shoot')
 export class shoot extends Component {
-    @property({type: Node})
-    Bow:Node = null;
-    
+
 
     @property({type: Prefab})
     Arrow:Prefab = null;
@@ -26,12 +24,9 @@ export class shoot extends Component {
     Head:Node = null;
 
     @property({type: Node})
-    canvasNode:Node = null;
-
-    @property({type: Node})
     ProgressBar:Node = null;
     angle: number;
-    speed: number=50;
+    speed: number=30;
 
     errow:Node; //  the arrow in which velocity is going to implement
     destroyErrow: Node; //the arror that has to remove
@@ -42,7 +37,7 @@ export class shoot extends Component {
 
 protected start(): void {
     input.on(Input.EventType.MOUSE_DOWN, this.onMouseCLick, this );
-    // input.on(Input.EventType.TOUCH_START, this.getTouchAngle, this);
+    input.on(Input.EventType.TOUCH_START, this.getTouchAngle, this);
 
 }
 
@@ -84,11 +79,29 @@ onMouseCLick(event: EventMouse){
 //get the angle of touch position
 getTouchAngle(event: EventTouch){
 
+    this.leftHand.angle = 0
+    this.Head.angle = 0
 
-    TouchPos = this.Bow.getComponent(UITransform).convertToNodeSpaceAR(v3(event.getUILocation().x, event.getUILocation().y,0))
+    TouchPos = this.leftHand.getComponent(UITransform).convertToNodeSpaceAR(v3(event.getUILocation().x, event.getUILocation().y,0))
     this.angle = (360+Math.round(180*Math.atan2(TouchPos.y, TouchPos.x)/Math.PI))%360;
-    console.log(this.angle, this.Bow.angle)
-    this.Bow.angle = this.angle
+
+    if(this.angle>maxRotatioDown && this.angle<maxRotationUp){
+        this.leftHand.angle = this.angle
+    }else if(this.angle>320){
+        this.leftHand.angle = this.angle
+    }else if(this.angle>180){
+        this.leftHand.angle = maxRotatioDown
+    }else if(this.angle>maxRotationUp){
+        this.leftHand.angle = maxRotationUp
+    }
+
+
+
+    this.leftHand.addChild(instantiate(this.Arrow)) // adding the errow as child of hand 
+    this.Head.angle = this.leftHand.angle //setting the head animation
+    this.errow = this.leftHand.children[this.leftHand.children.length-1] //putting the last errow 
+
+    this.SetArrowAngleToShoot(this.leftHand.angle)
 }
 
 
